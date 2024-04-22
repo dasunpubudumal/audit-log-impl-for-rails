d2053777901dba2cacf2c578e24c95371e629147
Dasun Pubudumal <pubudumald@gmail.com>
2024-04-22 11:51:11 +0100

    Simple role-based authentication (redundancies abound)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  enum role: { admin: 0, user: 1, guest: 2 }
   has_secure_password :password, validations: true
   validates :name, presence: true, uniqueness: true
+
+  def set_default_role
+    self.role ||= user!
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_101022) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_22_104318) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_101022) do
     t.string "password_confirmation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
